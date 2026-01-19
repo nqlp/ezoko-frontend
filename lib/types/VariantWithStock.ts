7505553d1c91a4be2c6bd6575507455474066a86
nqlp <nqlpaul@gmail.com>
2026-01-19 13:57:21 -0500

    fix: simplify warehouse_stock references structure in VariantWithStock type

--- lib/types/VariantWithStock.ts
+++ lib/types/VariantWithStock.ts
@@ -2,11 +2,9 @@ import { ProductVariant } from "./ProductVariant";
 import { MetaobjectNode } from "./MetaobjectNode";
 
 export type VariantWithStock = ProductVariant & {
-  warehouse_stock?: {
-    references?: {
-      edges: Array<{
-        node: MetaobjectNode;
-      }>;
+    warehouse_stock?: {
+        references?: {
+            node: MetaobjectNode;
+        };
     };
-  };
 };
