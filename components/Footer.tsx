aa1db9c3a702d03d5c3cab109968303ba2f0c7e6
nqlp <nqlpaul@gmail.com>
2026-01-20 10:21:11 -0500

    edit footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="w-full py-4 text-center text-sm text-gray-500">
-            © {new Date().getFullYear()} EZOKO. All rights reserved.
+            © {new Date().getFullYear()} EZOKO Fishing Inc. All rights reserved.
         </footer>
     );
 }
